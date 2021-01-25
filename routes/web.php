751d0f523abcfa354c6aed225b4e750b810557ce
Abdelrhman_Bayuomi <abdomet66@gmail.com>
2021-01-25 20:13:41 +0200

    last

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $player = \App\Player::where('PK_Player', 328)->first();
+    $player = \App\Player::where('PK_Player', 1772)->first();
     $item = \App\Item::where('PK_Item', 4)->first();
-    dd($player->items);
+    dd($player->charities);
 //    $player->charities;
 //    $player->items()->leftJoin('itemtype', 'itemtype.PK_ItemType','=','item.FK_ItemType')
 //        ->where('itemtype.IsImportant', 1)->get();
@@ -32,3 +32,6 @@ Route::get('/', function () {
 });
 
 Route::get('/playerKpis/{player_id}' , 'KpiController@index');
+
+Route::get('/malee' , 'KpiController@statistics');
+
