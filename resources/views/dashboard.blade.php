520f020b0d73bed072d8b887dad5a4e796733e08
Abdelrhman_Bayuomi <abdomet66@gmail.com>
2021-03-23 01:44:15 +0200

    last

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body class=" pt-md-3">
+
+<div class="container mt-md-5">
+    <iframe  height="612" src="https://app.powerbi.com/view?r=eyJrIjoiN2YzNzcwN2YtYjg5ZC00NGQzLWI2NWItODc0M2FmY2UyMGZiIiwidCI6IjMyNDlmZGUwLTA3YzgtNDNhNS04NTdmLTBjOWRlZjQ5YTYyOSJ9&pageName=ReportSection778904bbbb5a6f9e450f" frameborder="0" allowFullScreen="true"></iframe>
+</div>
+</body>
+</html>
