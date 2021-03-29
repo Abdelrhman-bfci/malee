41e5ad3a9397fa33003fa9783b7979c54df21b7d
Abdelrhman_Bayuomi <abdomet66@gmail.com>
2021-03-29 21:20:23 +0200

    last

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,10 +6,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
 </head>
-<body class=" pt-md-3">
-
-<div class="container mt-md-5">
-    <iframe  height="612" src="https://app.powerbi.com/view?r=eyJrIjoiN2YzNzcwN2YtYjg5ZC00NGQzLWI2NWItODc0M2FmY2UyMGZiIiwidCI6IjMyNDlmZGUwLTA3YzgtNDNhNS04NTdmLTBjOWRlZjQ5YTYyOSJ9&pageName=ReportSection778904bbbb5a6f9e450f" frameborder="0" allowFullScreen="true"></iframe>
+<body>
+<div class="container" style="width:100%; background:#ddd" >
+    <iframe class="responsive-iframe" src="https://app.powerbi.com/view?r=eyJrIjoiZDJmZjY4OTQtN2U5ZC00MDczLWFlNzktOGNkZTM4YTJmYjc0IiwidCI6IjMyNDlmZGUwLTA3YzgtNDNhNS04NTdmLTBjOWRlZjQ5YTYyOSJ9&pageName=ReportSection158a4705531e74b1b3b1" frameborder="0" allowFullScreen="true"></iframe>
 </div>
+<style>
+
+    body{
+        background-color: #ddddddb3;
+        margin: 0
+    }
+
+    .container {
+        position: relative;
+        overflow: hidden;
+        width: 100%;
+        padding-top: 45.25%; /* 16:9 Aspect Ratio (divide 9 by 16 = 0.5625) */
+    }
+
+    /* Then style the iframe to fit in the container div with full height and width */
+    .responsive-iframe {
+        position: absolute;
+        margin:0;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        width: 100%;
+        height: 100%;
+    }
+</style>
 </body>
 </html>
